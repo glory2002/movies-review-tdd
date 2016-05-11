9bad70efa8e0f288036c79944eeb65d8de2d0c4c
glory <glory.angelova@gmail.com>
2016-05-11 11:07:15 +0200

    rendering JSON for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,11 @@
 class MoviesController < ApplicationController
   def index
+    @movies = @category.movies
+
+    respond_to do |format|
+      format.html
+      format.json { render json: { category: @category, movies: @movies } }
+    end
   end
 
   def show
