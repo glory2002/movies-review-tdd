f5806115bb828be57fc938e2f9d312593eb7b61e
glory <glory.angelova@gmail.com>
2016-05-11 14:17:59 +0200

    all model test are passed

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,6 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   it {is_expected.to have_many :movies}
-  it {is_expected.to validates_presence_of :name}
-
+  it {is_expected.to validate_presence_of :name}
 end
